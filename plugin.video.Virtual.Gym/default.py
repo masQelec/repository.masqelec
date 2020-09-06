7a29acf59e626968988e2abbb632506c3f0d0713
masQchips <masqchips@gmail.com>
2020-09-06 17:31:56 +0200

    Add youtube and virtual.gym

---
+++ plugin.video.Virtual.Gym/default.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+			𝐕𝐈𝐑𝐓𝐔𝐀𝐋 𝐆𝐘𝐍 
+		𝐁𝐘 𝐍𝐄𝐓𝐀𝐈 𝐓𝐄𝐀𝐌 𝟐𝟎𝟐𝟎
+		
+"""
+
+import urllib , urllib2 , sys , re , os , unicodedata
+import xbmc , xbmcgui , xbmcplugin , xbmcaddon
+import cookielib , webbrowser
+import traceback , datetime , HTMLParser , httplib
+import resolveurl
+import cookielib , base64
+import requests
+import plugintools
+import codecs
+import start
