8c30d472a9f3b0ba4ce37daa95249959d37239b9
masqelec <masqelec@gmail.com>
2022-09-17 22:48:31 +0200

    Update

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -1,20 +1,20 @@
 import subprocess
 import urllib
+import xbmc
 
 def launch():
-	urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf", filename="/storage/.config/rclone/rclone.conf")
-	
-	try:
-		xbmc.log("Launching script", xbmc.LOGDEBUG)
-		subprocess.call(["systemctl", "daemon-reload"])      
-		subprocess.call(["systemctl", "restart", "rclone_iptv"])
-		subprocess.call(["systemctl", "restart", "rclone_recordings"])
-		subprocess.call(["systemctl", "restart", "rclone_tvshows_1"])
-		subprocess.call(["systemctl", "restart", "rclone_tvshows_2"])
-		subprocess.call(["systemctl", "restart", "rclone_videos_1"])
-		subprocess.call(["systemctl", "restart", "rclone_videos_2"])
-        
-	except:
-		xbmc.log("Failed to launch", xbmc.LOGDEBUG)
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf", filename="/storage/.config/rclone/rclone.conf")
+
+        try:
+                xbmc.log("Launching script", xbmc.LOGDEBUG)
+                subprocess.call(["systemctl", "daemon-reload"])
+                subprocess.call(["systemctl", "restart", "rclone_iptv"])
+                subprocess.call(["systemctl", "restart", "rclone_recordings"])
+                subprocess.call(["systemctl", "restart", "rclone_tvshows_1"])
+                subprocess.call(["systemctl", "restart", "rclone_tvshows_2"])
+                subprocess.call(["systemctl", "restart", "rclone_videos_1"])
+                subprocess.call(["systemctl", "restart", "rclone_videos_2"])
+        except:
+                xbmc.log("Failed to launch", xbmc.LOGDEBUG)
 
 launch()
