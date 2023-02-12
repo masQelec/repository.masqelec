5422aacac33485c6dc446a6f26d435b682bf4767
masQelec <masqelec@gmail.com>
2023-02-12 23:28:46 +0100

    update cloud

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -4,21 +4,26 @@ import subprocess
 import urllib.request
 
 def reload_rclone():
-	urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf", filename="/storage/.config/rclone/rclone.conf")
+    try:
+        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf",
+                                   filename="rclone.conf")
 
-	subprocess.call(["systemctl", "daemon-reload"])
-	subprocess.call(["systemctl", "restart", "rclone_tvshows_1"])
-	subprocess.call(["systemctl", "restart", "rclone_tvshows_2"])
-	subprocess.call(["systemctl", "restart", "rclone_videos_1"])
-	subprocess.call(["systemctl", "restart", "rclone_videos_2"])
+        subprocess.call(["systemctl", "daemon-reload"])
+        subprocess.call(["systemctl", "restart", "rclone_tvshows_1"])
+        subprocess.call(["systemctl", "restart", "rclone_tvshows_2"])
+        subprocess.call(["systemctl", "restart", "rclone_videos_1"])
+        subprocess.call(["systemctl", "restart", "rclone_videos_2"])
+
+    except:
+        time.sleep(5)
+        reload_rclone()
 
 reload_rclone()
 
 # Execute every 12 hours
 while True:
-     # Insert your code here
-     time.sleep(23600)
-     reload_rclone()
+    time.sleep(23600)
+    reload_rclone()
 
 
 
