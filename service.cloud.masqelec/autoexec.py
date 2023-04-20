94e379cee0a699722acc7f96599f481da3c773fc
masQelec <masqelec@gmail.com>
2023-04-20 22:36:58 +0200

    fix cloud service

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -5,7 +5,20 @@ import urllib
 
 def reload_rclone():
     try:
-        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf",filename="/storage/.config/rclone/rclone.conf")
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf",
+                                   filename="/storage/.config/rclone/rclone.conf")
+
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_tvshows_1.service",
+                                   filename="/storage/.config/system.d/rclone_tvshows_1.service")
+
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_tvshows_2.service",
+                                   filename="/storage/.config/system.d/rclone_tvshows_2.service")
+
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_videos_1.service",
+                                   filename="/storage/.config/system.d/rclone_videos_1.service")
+
+        urllib.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_videos_2.service",
+                                   filename="/storage/.config/system.d/rclone_videos_2.service")
 
         subprocess.call(["systemctl", "daemon-reload"])
         subprocess.call(["systemctl", "restart", "rclone_tvshows_1"])
