3b0453942fa93c20191c905149a18993a447a7e8
masQelec Mediacenter <masqelec@gmail.com>
2025-01-15 19:37:24 +0100

    update service.cloud

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -161,6 +161,51 @@ def reload_rclone():
     except:
         reload_rclone()
 
+# Función para modificar el agente de usuario en el archivo config
+def modificar_agente_usuario(ruta_archivo):
+    # Verificar si el archivo existe
+    if not os.path.isfile(ruta_archivo):
+        print(f"El archivo {ruta_archivo} no existe.")
+        return
+    
+    try:
+        # Leer el contenido del archivo
+        with open(ruta_archivo, 'r') as archivo:
+            lineas = archivo.readlines()
+        
+        cambio_realizado = False  # Variable para rastrear si se ha realizado un cambio
+
+        # Abrir el archivo en modo escritura para hacer los cambios
+        with open(ruta_archivo, 'w') as archivo:
+            for linea in lineas:
+                # Buscar la línea que contiene "http_user_agent"
+                if '"http_user_agent":' in linea:
+                    partes = linea.split('"http_user_agent":')
+                    
+                    # Obtener el valor actual del agente de usuario
+                    usuario_actual = partes[1].split('"')[1].strip()
+                    print(f'Agente de usuario actual: {usuario_actual}')  # Mensaje de depuración sin comilla adicional
+                    
+                    # Si el valor no es "samsung-agent/1.1", actualizarlo
+                    if usuario_actual != 'samsung-agent/1.1':
+                        nueva_linea = f'    "http_user_agent": "samsung-agent/1.1",\n'
+                        archivo.write(nueva_linea)
+                        cambio_realizado = True
+                        print("Se ha realizado un cambio.")  # Mensaje de depuración
+                    else:
+                        archivo.write(linea)
+                else:
+                    archivo.write(linea)
+        
+        if cambio_realizado:
+            # Mensaje de confirmación
+            print(f"El archivo {ruta_archivo} ha sido modificado.")
+        else:
+            print(f"No se realizaron cambios en el archivo {ruta_archivo}.")
+    
+    except Exception as e:
+        print(f"Ocurrió un error al modificar el archivo: {e}")
+        
 # Llamar a la función
 verificar_directorio_y_descargar_en_memoria('https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/service.libraryautoupdate.zip', '/storage/.kodi/addons/')
 
@@ -169,4 +214,7 @@ reload_rclone()
 
 # Llamar a la función
 update_version()
+
+# Llamada a la función con la ruta especificada
+modificar_agente_usuario('/storage/.kodi/userdata/addon_data/service.tvheadend43/config')
 
