657fc0e93608768a27a12cd9467cc9e9a7871279
masQelec Mediacenter <masqelec@gmail.com>
2025-03-06 04:18:50 +0100

    update repo

--- plugin.video.elementum/scripts/xgettext_get_available_ids.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o nounset
-set -o errexit
-
-firstID=$(grep msgctxt resources/language/messages.pot | grep -oP '\d+' | head -1)
-lastID=$(grep msgctxt resources/language/messages.pot | grep -oP '\d+' | tail -1)
-
-seq -w $firstID $lastID | grep -vwFf <(grep msgctxt resources/language/messages.pot | grep -oP '\d+')
