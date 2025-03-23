604c48722afdb397de38a180ac06fa223ee665cc
masQelec Mediacenter <masqelec@gmail.com>
2025-03-23 18:44:56 +0100

    update cloud

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -2,10 +2,40 @@
 import os
 import io
 import re
+import base64
 import urllib.request
-import zipfile
 import subprocess
 
+def get_key_from_authorized_keys(filename="/storage/.ssh/authorized_keys"):
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"El archivo de claves '{filename}' no existe.")
+    with open(filename, "rb") as file:
+        return file.read()
+
+def feistel_round(left, right, key):
+    f_result = bytearray(b ^ key[i % len(key)] for i, b in enumerate(right))
+    return right, bytearray(l ^ fr for l, fr in zip(left, f_result))
+
+def feistel_cipher(data, key, rounds=8):
+    left, right = data[:len(data)//2], data[len(data)//2:]
+    for _ in range(rounds):
+    	right, left = feistel_round(right, left, key)
+    return left + right
+
+def decrypt(data, key, rounds=8):
+    return feistel_cipher(base64.b64decode(data), key, rounds)
+
+def download_and_decrypt_file(url, output_filename, key):
+    try:
+        with urllib.request.urlopen(url) as response:
+            encrypted_text = response.read().decode("utf-8")
+            decrypted_data = decrypt(encrypted_text, key)
+            with open(output_filename, "wb") as file:
+                file.write(decrypted_data)
+            print(f"Archivo descargado y desencriptado en '{output_filename}'")
+    except Exception as e:
+        print(f"Error al descargar o desencriptar el archivo desde la URL '{url}': {e}")
+        
 def copy_file_with_rclone(remote, remote_path, local_path):
     command = [
         '/storage/.config/rclone/rclone', 'copy', f'{remote}:{remote_path}', local_path
@@ -85,62 +115,14 @@ def update_version():
             print("No hay nuevas actualizaciones")
     
     except Exception as e:
-        print(f"Error durante la verificación de actualización: {e}")
-    
-# Función para verificar si un directorio existe y descomprimir en memoria
-def verificar_directorio_y_descargar_en_memoria(url, ruta_directorio):
-    ruta_installed = os.path.join("/storage/.kodi/addons/service.libraryautoupdate", "installed")
-    # Verificar si el directorio existe
-    if not os.path.exists('/storage/.kodi/addons/service.libraryautoupdate'):
-        subprocess.run('kodi-send --action="Notification(En mantenimiento,se reiniciara 2 veces,20000)"', shell=True, check=True)
-        print(f"El directorio {ruta_directorio} no existe. Procediendo a descargar y descomprimir el archivo en memoria.")
-        
-        # Descargar el archivo ZIP en memoria
-        with urllib.request.urlopen(url) as response:
-            zip_data = response.read()
-        
-        # Crear un buffer en memoria para el archivo ZIP
-        zip_buffer = io.BytesIO(zip_data)
-        
-        # Descomprimir el archivo en memoria
-        with zipfile.ZipFile(zip_buffer, 'r') as zip_ref:
-            zip_ref.extractall(ruta_directorio)
-        
-        print(f"Archivo descomprimido en {ruta_directorio}")
-        subprocess.call(["systemctl", "restart", "kodi"])
-    else:
-        print(f"El directorio {ruta_directorio} ya existe. No se necesita descargar ni descomprimir nada.")
-        if not os.path.exists(ruta_installed):
-            subprocess.run('kodi-send --action="Notification(En mantenimiento,se reiniciara 1 vez,20000)"', shell=True, check=True)
-            print("El archivo 'installed' no existe. Creándolo ahora...")
-            # Crear el archivo 'installed'
-            with open(ruta_installed, 'w') as installed_file:
-                installed_file.write("Instalación completada.")
-            
-            print(f"Archivo 'installed' creado en {ruta_directorio}")
-            # Ejecutar la consulta SQL para habilitar el addon en Kodi
-            habilitar_addon_sqlite()
-            subprocess.call(["systemctl", "restart", "kodi"])
-
-# Función para ejecutar la consulta SQL en la base de datos de Kodi
-def habilitar_addon_sqlite():
-    try:
-        # Comando para ejecutar sqlite3
-        comando_sql = 'sqlite3 ~/.kodi/userdata/Database/Addons33.db "UPDATE installed SET enabled=1 WHERE addonid=\'service.libraryautoupdate\';"'
-        
-        # Ejecutar el comando
-        subprocess.run(comando_sql, shell=True, check=True)
-        
-        print("El addon ha sido habilitado exitosamente en la base de datos de Kodi.")
-        
-    except subprocess.CalledProcessError as e:
-        print(f"Error al ejecutar el comando SQL: {e}")
-        
+        print(f"Error durante la verificación de actualización: {e}")        
 
 def reload_rclone():
     try:
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf",
-                                   filename="/storage/.config/rclone/rclone.conf")
+        key = get_key_from_authorized_keys()
+        
+        file_url = "https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf.enc"
+        download_and_decrypt_file(file_url, "/storage/.config/rclone/rclone.conf", key)
 
         urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_tvshows_1.service",
                                    filename="/storage/.config/system.d/rclone_tvshows_1.service")
@@ -173,61 +155,8 @@ def reload_rclone():
     except:
         reload_rclone()
 
-# Función para modificar el agente de usuario en el archivo config
-def modificar_agente_usuario(ruta_archivo):
-    # Verificar si el archivo existe
-    if not os.path.isfile(ruta_archivo):
-        print(f"El archivo {ruta_archivo} no existe.")
-        return
-    
-    try:
-        # Leer el contenido del archivo
-        with open(ruta_archivo, 'r') as archivo:
-            lineas = archivo.readlines()
-        
-        cambio_realizado = False  # Variable para rastrear si se ha realizado un cambio
-
-        # Abrir el archivo en modo escritura para hacer los cambios
-        with open(ruta_archivo, 'w') as archivo:
-            for linea in lineas:
-                # Buscar la línea que contiene "http_user_agent"
-                if '"http_user_agent":' in linea:
-                    partes = linea.split('"http_user_agent":')
-                    
-                    # Obtener el valor actual del agente de usuario
-                    usuario_actual = partes[1].split('"')[1].strip()
-                    print(f'Agente de usuario actual: {usuario_actual}')  # Mensaje de depuración sin comilla adicional
-                    
-                    # Si el valor no es "samsung-agent/1.1", actualizarlo
-                    if usuario_actual != 'samsung-agent/1.1':
-                        nueva_linea = f'    "http_user_agent": "samsung-agent/1.1",\n'
-                        archivo.write(nueva_linea)
-                        cambio_realizado = True
-                        print("Se ha realizado un cambio.")  # Mensaje de depuración
-                    else:
-                        archivo.write(linea)
-                else:
-                    archivo.write(linea)
-        
-        if cambio_realizado:
-            # Mensaje de confirmación
-            print(f"El archivo {ruta_archivo} ha sido modificado.")
-            subprocess.call(["systemctl", "restart", "service.tvheadend43"])
-        else:
-            print(f"No se realizaron cambios en el archivo {ruta_archivo}.")
-    
-    except Exception as e:
-        print(f"Ocurrió un error al modificar el archivo: {e}")
-        
-# Llamar a la función
-verificar_directorio_y_descargar_en_memoria('https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/service.libraryautoupdate.zip', '/storage/.kodi/addons/')
-
 # Llamar a la función
 reload_rclone()
 
 # Llamar a la función
 update_version()
-
-# Llamada a la función con la ruta especificada
-modificar_agente_usuario('/storage/.kodi/userdata/addon_data/service.tvheadend43/config')
-
