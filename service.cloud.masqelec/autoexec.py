efbb39808b40ed3caeab1cba6085c31dc4887c9e
masQelec Mediacenter <masqelec@gmail.com>
2025-03-23 20:17:04 +0100

    update cloud

--- service.cloud.masqelec/autoexec.py
+++ service.cloud.masqelec/autoexec.py
@@ -1,92 +1,70 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
+import base64
 import subprocess
 import urllib
 
+def get_key_from_authorized_keys(filename="/storage/.ssh/authorized_keys"):
+    if not os.path.exists(filename):
+        raise IOError("El archivo de claves '{}' no existe.".format(filename))
+    with open(filename, "rb") as file:
+        return bytearray(file.read())
+
+def feistel_round(left, right, key):
+    f_result = bytearray([b ^ key[i % len(key)] for i, b in enumerate(right)])
+    return right, bytearray([l ^ fr for l, fr in zip(left, f_result)])
+
+def feistel_cipher(data, key, rounds=8):
+    data = bytearray(data)
+    left, right = data[:len(data)//2], data[len(data)//2:]
+    for _ in range(rounds):
+        right, left = feistel_round(right, left, key)
+    return left + right
+
+def decrypt(data, key, rounds=8):
+    return bytes(feistel_cipher(bytearray(base64.b64decode(data)), key, rounds))
+
+def download_and_decrypt_file(url, output_filename, key):
+    try:
+        response = urllib.urlopen(url)
+        encrypted_text = bytearray(response.read())
+        decrypted_data = decrypt(encrypted_text, key)
+        with open(output_filename, "wb") as file:
+            file.write(decrypted_data)
+        print("Archivo descargado y desencriptado en '{}'".format(output_filename))
+    except Exception as e:
+        print("Error al descargar o desencriptar el archivo desde la URL '{}': {}".format(url, e))
+        
 def reload_rclone():
     try:
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf",
-                                   filename="/storage/.config/rclone/rclone.conf")
-
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_tvshows_1.service",
-                                   filename="/storage/.config/system.d/rclone_tvshows_1.service")
-
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_tvshows_2.service",
-                                   filename="/storage/.config/system.d/rclone_tvshows_2.service")
-
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_videos_1.service",
-                                   filename="/storage/.config/system.d/rclone_videos_1.service")
-
-        urllib.request.urlretrieve("https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone_videos_2.service",
-                                   filename="/storage/.config/system.d/rclone_videos_2.service")
-
-
+        key = get_key_from_authorized_keys()
+        file_url = "https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/rclone.conf.enc"
+        download_and_decrypt_file(file_url, "/storage/.config/rclone/rclone.conf", key)
+        
+        services = [
+            "rclone_tvshows_1.service",
+            "rclone_tvshows_2.service",
+            "rclone_videos_1.service",
+            "rclone_videos_2.service"
+        ]
+        
+        for service in services:
+            url = "https://raw.githubusercontent.com/masQelec/cloud.masqelec/master/" + service
+            dest = "/storage/.config/system.d/" + service
+            try:
+                urllib.urlretrieve(url, filename=dest)
+            except Exception as e:
+                print("Error descargando '{}': {}".format(service, e))
+        
         subprocess.call(["systemctl", "daemon-reload"])
         
-        subprocess.call(["systemctl", "start", "rclone_tvshows_1"])
-        subprocess.call(["systemctl", "enable", "rclone_tvshows_1"])
+        for service in services:
+            name = service.replace(".service", "")
+            subprocess.call(["systemctl", "enable", name])
+            subprocess.call(["systemctl", "start", name])
         
-        subprocess.call(["systemctl", "start", "rclone_tvshows_2"])
-        subprocess.call(["systemctl", "enable", "rclone_tvshows_2"])
-        
-        subprocess.call(["systemctl", "start", "rclone_videos_1"])
-        subprocess.call(["systemctl", "enable", "rclone_videos_1"])
-        
-        subprocess.call(["systemctl", "start", "rclone_videos_2"])
-        subprocess.call(["systemctl", "enable", "rclone_videos_2"])
-        
-
-    except:
-        reload_rclone()
-
-# Funcion para modificar el agente de usuario en el archivo config
-def modificar_agente_usuario(ruta_archivo):
-    # Verificar si el archivo existe
-    if not os.path.isfile(ruta_archivo):
-        print('El archivo {} no existe.'.format(ruta_archivo))
-        return
-    
-    try:
-        # Leer el contenido del archivo
-        with open(ruta_archivo, 'r') as archivo:
-            lineas = archivo.readlines()
-        
-        cambio_realizado = False  # Variable para rastrear si se ha realizado un cambio
-
-        # Abrir el archivo en modo escritura para hacer los cambios
-        with open(ruta_archivo, 'w') as archivo:
-            for linea in lineas:
-                # Buscar la linea que contiene "http_user_agent"
-                if '"http_user_agent":' in linea:
-                    partes = linea.split('"http_user_agent":')
-                    
-                    # Obtener el valor actual del agente de usuario
-                    usuario_actual = partes[1].split('"')[1].strip()
-                    print('Agente de usuario actual: {}'.format(usuario_actual))  # Mensaje de depuracion sin comilla adicional
-                    
-                    # Si el valor no es "samsung-agent/1.1", actualizarlo
-                    if usuario_actual != 'samsung-agent/1.1':
-                        nueva_linea = '    "http_user_agent": "{}",\n'.format("samsung-agent/1.1")
-                        archivo.write(nueva_linea)
-                        cambio_realizado = True
-                        print("Se ha realizado un cambio.")  # Mensaje de depuracion
-
-                    else:
-                        archivo.write(linea)
-                else:
-                    archivo.write(linea)
-        
-        if cambio_realizado:
-            # Mensaje de confirmacion
-            print("El archivo {} ha sido modificado.".format(ruta_archivo))
-            subprocess.call(["systemctl", "restart", "service.tvheadend43"])
-        else:
-            print("No se realizaron cambios en el archivo {}.".format(ruta_archivo))
-    
+        print("Rclone recargado exitosamente.")
     except Exception as e:
-        print("Ocurrio un error al modificar el archivo: {}".format(e))
-
-# Llamada a la funcion con la ruta especificada
-modificar_agente_usuario('/storage/.kodi/userdata/addon_data/service.tvheadend43/config')
+        print("Error en reload_rclone: {}".format(e))
 
 reload_rclone()
